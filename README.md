f8308947e46cae0255cd91f3ba913de08a5ba0ee
nano-nano <nano2.aloerina@gmail.com>
2020-08-25 08:36:30 +0900

    README.mdとAboutページ更新

--- README.md
+++ README.md
@@ -1,24 +1,46 @@
 # vbc on Web
 
-## Project setup
+## これはなに？
+
+バーチャルにクイズ大会「abc」を再現できるPython3向けツール「vbc」にインスパイアを受け、  
+同等の処理をWeb上で行えるように取り組んだWebアプリです。
+
+本家のvbcについては[こちら](https://github.com/NMLibrary/vbc)
+
+クイズ大会「abc」については[こちら](http://abc-dive.com/)
+
+## ライセンス
+
+本リポジトリで公開されているコードはMITライセンスの元、利用可能です。
+
+著作権表示については
 ```
-yarn install
+Copyright (c) 2020 Nano-Nano
+```
+とします。
+
+## 作成者/問い合わせ先
+
+[Banno Takuya/なのなの(@nano2_aloerina)](https://twitter.com/nano2_aloerina)
+
+## 開発者向け情報
+
+### Project setup
+```
+$ yarn install
 ```
 
 ### Compiles and hot-reloads for development
 ```
-yarn serve
+$ yarn serve
 ```
 
 ### Compiles and minifies for production
 ```
-yarn build
+$ yarn build
 ```
 
 ### Lints and fixes files
 ```
-yarn lint
+$ yarn lint
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/About.vue
+++ src/views/About.vue
@@ -7,7 +7,20 @@
       <p>
         このサイトは、バーチャルにクイズ大会「abc」を再現できるPython3向けツール「vbc」にインスパイアを受け、
         同等の処理をWeb上で行えるように取り組んだWebアプリです。<br/>
-        本家のvbcについては<a href="https://github.com/NMLibrary/vbc">こちら</a>
+        本家のvbcについては<a href="https://github.com/NMLibrary/vbc">こちら</a><br/>
+        クイズ大会「abc」については<a href="http://abc-dive.com/">こちら</a>
+      </p>
+
+      <h3>使い方</h3>
+      <p>
+        「参加者データCSVファイル」のフォーム内にある<strong>ファイル選択</strong>ボタンでCSVファイルを読み込むか、
+        CSVファイルを直接ドラッグ＆ドロップしてください。<br/>
+        CSVファイルの形式は<a href="https://github.com/NMLibrary/vbc#sample_datacsv%E3%81%AE%E5%BD%A2%E5%BC%8F%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6">本家vbcのREADME.md</a>をご覧ください。
+      </p>
+      <p>
+        ＜拡張要素について＞<br/>
+        本家vbcのCSVデータを拡張し、<strong>10 up-downのコース希望</strong>のうしろにカンマ区切りで「所属」を設定できるようにしています。
+        （未設定の場合は所属は空欄です）
       </p>
 
       <h3>注意事項・免責事項</h3>
