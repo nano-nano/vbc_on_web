540944c1b190ed62ae6a22a9f36eb0dfb4b0c334
Nano-Nano <nano2.aloerina@gmail.com>
2020-09-16 17:21:16 +0900

    結果画像保存機能を追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,80 +19,83 @@
         <!-- インポート完了後の結果表示 -->
         <div v-if="state.isFileLoaded">
           <div class="item-bottom-margin d-flex justify-content-end">
+            <b-button variant="outline-secondary" class="ml-2" @click="onCaptureClicked">表示をキャプチャ</b-button>
             <b-button variant="danger" class="ml-2" @click="onClearClicked">結果をクリア</b-button>
           </div>
 
-          <!-- ペーパー -->
-          <b-card no-body class="item-bottom-margin">
-            <b-card-header header-tag="header" class="p-1" role="tab">
-              <b-button block v-b-toggle.accordion-paper variant="secondary">Round 1: ペーパークイズ 結果</b-button>
-            </b-card-header>
-            <b-collapse id="accordion-paper" visible role="tabpanel">
-              <b-card-body>
-                <PaperResult :playerList="state.playerdataList"/>
-              </b-card-body>
-            </b-collapse>
-          </b-card>
+          <div id="result-content">
+            <!-- ペーパー -->
+            <b-card no-body class="item-bottom-margin">
+              <b-card-header header-tag="header" class="p-1" role="tab">
+                <b-button block v-b-toggle.accordion-paper variant="secondary">Round 1: ペーパークイズ 結果</b-button>
+              </b-card-header>
+              <b-collapse id="accordion-paper" visible role="tabpanel">
+                <b-card-body>
+                  <PaperResult :playerList="state.playerdataList"/>
+                </b-card-body>
+              </b-collapse>
+            </b-card>
 
-          <!-- 2R -->
-          <b-card no-body class="item-bottom-margin">
-            <b-card-header header-tag="header" class="p-1" role="tab">
-              <b-button block v-b-toggle.accordion-r2 variant="secondary">Round 2: 連答つき５○２× 結果</b-button>
-            </b-card-header>
-            <b-collapse id="accordion-r2" role="tabpanel">
-              <b-card-body>
-                <Round2Result :playerList="state.playerdataList" @onFinish2r="onFinish2r" />
-              </b-card-body>
-            </b-collapse>
-          </b-card>
+            <!-- 2R -->
+            <b-card no-body class="item-bottom-margin">
+              <b-card-header header-tag="header" class="p-1" role="tab">
+                <b-button block v-b-toggle.accordion-r2 variant="secondary">Round 2: 連答つき５○２× 結果</b-button>
+              </b-card-header>
+              <b-collapse id="accordion-r2" role="tabpanel">
+                <b-card-body>
+                  <Round2Result :playerList="state.playerdataList" @onFinish2r="onFinish2r" />
+                </b-card-body>
+              </b-collapse>
+            </b-card>
 
-          <!-- 3R -->
-          <b-card no-body class="item-bottom-margin">
-            <b-card-header header-tag="header" class="p-1" role="tab">
-              <b-button block v-b-toggle.accordion-r3 variant="secondary">Round 3: Number 10 結果</b-button>
-            </b-card-header>
-            <b-collapse id="accordion-r3" role="tabpanel">
-              <b-card-body>
-                <Round3Result :playerList="state.playerdataList" />
-              </b-card-body>
-            </b-collapse>
-          </b-card>
+            <!-- 3R -->
+            <b-card no-body class="item-bottom-margin">
+              <b-card-header header-tag="header" class="p-1" role="tab">
+                <b-button block v-b-toggle.accordion-r3 variant="secondary">Round 3: Number 10 結果</b-button>
+              </b-card-header>
+              <b-collapse id="accordion-r3" role="tabpanel">
+                <b-card-body>
+                  <Round3Result :playerList="state.playerdataList" />
+                </b-card-body>
+              </b-collapse>
+            </b-card>
 
-          <!-- ExR -->
-          <b-card no-body class="item-bottom-margin">
-            <b-card-header header-tag="header" class="p-1" role="tab">
-              <b-button block v-b-toggle.accordion-ex variant="secondary">Extra Round: 敗者復活 結果</b-button>
-            </b-card-header>
-            <b-collapse id="accordion-ex" role="tabpanel">
-              <b-card-body>
-                <ExRResult :playerList="state.playerdataList" />
-              </b-card-body>
-            </b-collapse>
-          </b-card>
+            <!-- ExR -->
+            <b-card no-body class="item-bottom-margin">
+              <b-card-header header-tag="header" class="p-1" role="tab">
+                <b-button block v-b-toggle.accordion-ex variant="secondary">Extra Round: 敗者復活 結果</b-button>
+              </b-card-header>
+              <b-collapse id="accordion-ex" role="tabpanel">
+                <b-card-body>
+                  <ExRResult :playerList="state.playerdataList" />
+                </b-card-body>
+              </b-collapse>
+            </b-card>
 
-          <!-- SF -->
-          <b-card no-body class="item-bottom-margin">
-            <b-card-header header-tag="header" class="p-1" role="tab">
-              <b-button block v-b-toggle.accordion-sf variant="secondary">Semi Final: 3セット制タイムレース Nine Hundred 結果</b-button>
-            </b-card-header>
-            <b-collapse id="accordion-sf" role="tabpanel">
-              <b-card-body>
-                <SemiFinalResult :playerList="state.playerdataList" />
-              </b-card-body>
-            </b-collapse>
-          </b-card>
+            <!-- SF -->
+            <b-card no-body class="item-bottom-margin">
+              <b-card-header header-tag="header" class="p-1" role="tab">
+                <b-button block v-b-toggle.accordion-sf variant="secondary">Semi Final: 3セット制タイムレース Nine Hundred 結果</b-button>
+              </b-card-header>
+              <b-collapse id="accordion-sf" role="tabpanel">
+                <b-card-body>
+                  <SemiFinalResult :playerList="state.playerdataList" />
+                </b-card-body>
+              </b-collapse>
+            </b-card>
 
-          <!-- F -->
-          <b-card no-body class="item-bottom-margin">
-            <b-card-header header-tag="header" class="p-1" role="tab">
-              <b-button block v-b-toggle.accordion-f variant="secondary">Final: トリプルセブン 結果</b-button>
-            </b-card-header>
-            <b-collapse id="accordion-f" role="tabpanel">
-              <b-card-body>
-                <FinalResult :playerList="state.playerdataList" />
-              </b-card-body>
-            </b-collapse>
-          </b-card>
+            <!-- F -->
+            <b-card no-body class="item-bottom-margin">
+              <b-card-header header-tag="header" class="p-1" role="tab">
+                <b-button block v-b-toggle.accordion-f variant="secondary">Final: トリプルセブン 結果</b-button>
+              </b-card-header>
+              <b-collapse id="accordion-f" role="tabpanel">
+                <b-card-body>
+                  <FinalResult :playerList="state.playerdataList" />
+                </b-card-body>
+              </b-collapse>
+            </b-card>
+          </div>
         </div>
       </div>
     </div>
@@ -101,6 +104,10 @@
 
 <script lang="ts">
 import { defineComponent, reactive } from "@vue/composition-api";
+import { Observable, Observer } from 'rxjs';
+import html2canvas from 'html2canvas';
+import moment from 'moment';
+
 import { PlayerEntity } from '@/vbc-entity'
 import NavBar from '@/components/NavBar.vue';
 import CsvImportView from '@/components/Home/CsvImportView.vue';
@@ -110,7 +117,6 @@ import Round3Result from '@/components/Home/3RResult.vue';
 import ExRResult from '@/components/Home/ExRResult.vue';
 import SemiFinalResult from '@/components/Home/SFResult.vue';
 import FinalResult from '@/components/Home/FResult.vue';
-import { Observable, Observer } from 'rxjs';
 
 export default defineComponent({
   components: {
@@ -153,6 +159,27 @@ export default defineComponent({
       }
     }
 
+    const onCaptureClicked = () => {
+      const target = document.querySelector('#result-content') as HTMLElement;
+      if (target == null) return;
+      html2canvas(target).then(canvas => {
+        canvas.toBlob(blob => {
+          const fileName = `vbc_result_${moment().format('YYYYMMDDHHmmss')}.png`;
+
+          if (window.navigator.msSaveBlob != undefined) {
+            // IEやEdgeの場合
+            window.navigator.msSaveOrOpenBlob(blob, fileName);
+          } else {
+            const url = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = fileName;
+            link.click();
+          }
+        });
+      });
+    }
+
     const onFinish2r = (logStr: string) => {
       state.vbcLogList.push({ round: '2R', str: logStr });
     }
@@ -162,6 +189,7 @@ export default defineComponent({
       clearObservable,
       onImportFileSelected,
       onClearClicked,
+      onCaptureClicked,
       onFinish2r
     }
   }
